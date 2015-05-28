5f72317405cbb1e8024e0dac445372a7210aa805
mizukami <mizukami@131.206.23.158>
2015-05-28 11:02:47 +0900

    変更

--- src/gittest1/Gittest1.java
+++ src/gittest1/Gittest1.java
@@ -1,22 +1,19 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editgy.
  */
 package gittest1;
 
-/**
- *
- * @author mizukami
- */
 public class Gittest1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    System.out.print("Hello World");
+        
+    System.out.print("AA");
+    System.out.print("BB");
     }
     
 }
